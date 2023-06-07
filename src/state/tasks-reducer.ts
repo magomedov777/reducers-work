5836d0bbf1751e946c3bcaecb577c7b8ef078647
Arkadiy <alibullterrier@gmail.com>
2023-06-07 06:53:58 +0300

    action creator

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -63,8 +63,6 @@ export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: 
     return {type: 'CHANGE-TASK-STATUS', taskId, isDone, todolistId}as const
 }
 
-export const changeTaskTitleAC = (taskId: string, title: string, todolistId: string) => {
-    return {type: 'CHANGE-TASK-TITLE', taskId, title, todolistId}as const
-}
+
 
 
