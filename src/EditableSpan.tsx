3c6aa7af79b6a39c02e473b81f062270608152fa
Arkadiy <alibullterrier@gmail.com>
2023-06-21 06:16:59 +0300

    view

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -11,7 +11,10 @@ export function EditableSpan(props: EditableSpanPropsType) {
     let [editMode, setEditMode] = useState(false);
     let [title, setTitle] = useState(props.value);
 
-    
+    const activateEditMode = () => {
+        setEditMode(true);
+        setTitle(props.value);
+    }
     const activateViewMode = () => {
         setEditMode(false);
         props.onChange(title);
