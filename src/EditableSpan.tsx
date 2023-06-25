090b550f2defb7f0d9e0fc10c8e3a69975bd8912
Arkadiy <alibullterrier@gmail.com>
2023-06-25 09:43:01 +0300

    set edit mode, useState

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -15,7 +15,10 @@ export function EditableSpan(props: EditableSpanPropsType) {
         setEditMode(true);
         setTitle(props.value);
     }
-    
+    const activateViewMode = () => {
+        setEditMode(false);
+        props.onChange(title);
+    }
     const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
     }
