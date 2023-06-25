ad41a995cb04080250a82fe52205d1af476a6cbb
Arkadiy <alibullterrier@gmail.com>
2023-06-25 09:42:08 +0300

    new edit mode

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -8,7 +8,8 @@ type EditableSpanPropsType = {
 }
 
 export function EditableSpan(props: EditableSpanPropsType) {
-    
+    let [editMode, setEditMode] = useState(false);
+    let [title, setTitle] = useState(props.value);
 
     const activateEditMode = () => {
         setEditMode(true);
