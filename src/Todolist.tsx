ca888150f443a4ef2aeeb1b0335966720f9061a0
Arkadiy <alibullterrier@gmail.com>
2023-06-30 06:22:04 +0300

    refactor

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -40,7 +40,7 @@ export function Todolist(props: PropsType) {
         props.changeTodolistTitle(props.id, title);
     }
 
-    const onAllClickHandler = () => props.changeFilter("all", );
+    const onAllClickHandler = () => props.changeFilter("all", props.id);
     const onActiveClickHandler = () => props.changeFilter("active", props.id);
     const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
 
