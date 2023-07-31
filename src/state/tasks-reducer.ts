6d58ba132c77aeb678793e7764ba5a1f25fcad66
Arkadiy <alibullterrier@gmail.com>
2023-07-31 06:21:28 +0300

    return copy

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -44,7 +44,6 @@ export const TasksReducer = (state = initialState, action: ActionsType) => {
         case 'REMOVE-TODOLIST': {
             let copyState = {...state}
             delete copyState[action.payload.todolistId]
-            return copyState
         }
        
         default: return state
