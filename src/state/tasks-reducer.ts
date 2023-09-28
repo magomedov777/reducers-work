9e1b2aad01c0bdc27e7a50d9b1c83ccde1341dac
Arkadiy <alibullterrier@gmail.com>
2023-09-28 06:55:21 +0300

    create: remove task & add task Action Creator

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -19,6 +19,16 @@ const initialState: TasksStateType = {};
 
 export const TasksReducer = (state = initialState, action: ActionsType) => {
   switch (action.type) {
+    case "REMOVE-TASK":
+      return {
+        ...state,
+        [action.todolistId]: state[action.todolistId].filter((t) => t.id !== action.taskId),
+      };
+    case "ADD-TASK":
+      return {
+        ...state,
+        [action.todolistId]: [{ id: v1(), title: action.title, isDone: false }, ...state[action.todolistId]],
+      };
     case "CHANGE-TASK-STATUS":
       return {
         ...state,
@@ -47,6 +57,14 @@ export const TasksReducer = (state = initialState, action: ActionsType) => {
     default:
       return state;
   }
+};
+
+export const removeTaskAC = (taskId: string, todolistId: string) => {
+  return { type: "REMOVE-TASK", taskId, todolistId } as const;
+};
+
+export const addTaskAC = (title: string, todolistId: string) => {
+  return { type: "ADD-TASK", title, todolistId } as const;
 };
 
 export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: string) => {
