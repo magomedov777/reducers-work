f18720a66287e0b1437e7d03c7d8000dadc4e900
Arkadiy <alibullterrier@gmail.com>
2023-09-29 06:20:32 +0300

    create: actions for CRUD operation todolist

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -14,3 +14,48 @@ export type MainTypeAction =
   | changeTodolistTitleACType
   | changeFilterACType
   | addTodolistACType;
+
+export type removeTodolistACType = ReturnType<typeof removeTodolistAC>;
+export const removeTodolistAC = (todolistId: string) => {
+  return {
+    type: "REMOVE-TODOLIST",
+    payload: {
+      todolistId,
+    },
+  } as const;
+};
+
+export type addTodolistACType = ReturnType<typeof addTodolistAC>;
+
+export const addTodolistAC = (newTitle: string) => {
+  return {
+    type: "ADD-TODOLIST",
+    payload: {
+      newTitle,
+      todolistId: v1(),
+    },
+  } as const;
+};
+
+export type changeTodolistTitleACType = ReturnType<typeof changeTodolistTitleAC>;
+
+export const changeTodolistTitleAC = (todolistID: string, newTitle: string) => {
+  return {
+    type: "CHANGE-TODOLIST-TITLE",
+    payload: {
+      todolistID,
+      newTitle,
+    },
+  } as const;
+};
+
+export type changeFilterACType = ReturnType<typeof changeFilterAC>;
+export const changeFilterAC = (todolistID: string, filterValue: FilterValuesType) => {
+  return {
+    type: "CHANGE-TODOLIST-FILTER",
+    payload: {
+      todolistID,
+      filterValue,
+    },
+  } as const;
+};
