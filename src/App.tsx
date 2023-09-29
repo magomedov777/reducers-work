79d5644611916f0f6792dc480937367fc880afbf
Arkadiy <alibullterrier@gmail.com>
2023-09-30 00:04:16 +0300

    add modified state for App

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,24 @@ export type TasksStateType = {
 
 
 function App() {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
 
+    let [todolists, setTodolists] = useState<Array<TodolistType>>([
+        { id: todolistId1, title: "What to learn", filter: "all" },
+        { id: todolistId2, title: "What to buy", filter: "all" }
+    ])
+
+    let [tasks, setTasks] = useState<TasksStateType>({
+        [todolistId1]: [
+            { id: v1(), title: "HTML&CSS", isDone: true },
+            { id: v1(), title: "JS", isDone: true }
+        ],
+        [todolistId2]: [
+            { id: v1(), title: "Milk", isDone: true },
+            { id: v1(), title: "React Book", isDone: true }
+        ]
+    });
 
 
     function removeTask(id: string, todolistId: string) {
